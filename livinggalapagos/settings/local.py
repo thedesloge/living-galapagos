19c7dd496f688d44e3dfc7398294d07447374718
caseymm <caseymm@gmail.cpom>
2013-03-28 21:15:54 -0400

    slight article change

--- livinggalapagos/settings/local.py
+++ livinggalapagos/settings/local.py
@@ -14,11 +14,11 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+        'NAME': 'casey',                      # Or path to database file if using sqlite3.
+        'USER': 'casey',                      # Not used with sqlite3.
+        'PASSWORD': 'gottaloveshane',                  # Not used with sqlite3.
+        'HOST': 'localhost',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '5432',                      # Set to empty string for default. Not used with sqlite3.
     }
 }
 
