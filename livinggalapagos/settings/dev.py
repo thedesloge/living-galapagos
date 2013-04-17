e8159aa1a53651bed625b0ad051db5cb9ff82a13
Donald Desloge <desloge@unc.edu>
2013-04-17 15:16:33 +0000

    update dev settings

--- livinggalapagos/settings/dev.py
+++ livinggalapagos/settings/dev.py
@@ -25,7 +25,7 @@ MEDIA_URL = 'http://uncviscom.webfactional.com/livinggalapagos/media/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = '/home/uncviscom/webapps/static_media/'
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
