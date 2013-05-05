4cfe14ec76da4dc687f69b002611b65ed908510f
caseymm <caseymm@gmail.cpom>
2013-05-05 11:04:21 -0400

    twitter working again

--- story_database/static/js/jquery.jtweetsanywhere-1.3.1.js
+++ story_database/static/js/jquery.jtweetsanywhere-1.3.1.js
@@ -248,7 +248,7 @@ JTA_I18N.addResourceBundle('jTweetsAnywhere', 'en',
 			 * The name of a user's list where the tweet feed is generated from. The special
 			 * list name 'favorites' can be used to display a user's favorited tweets.
 			 */
-			list: 'galapagos',
+			list: null,
 
 			/**
 			 * A single search param string or an array of search params, to be used in
@@ -258,7 +258,7 @@ JTA_I18N.addResourceBundle('jTweetsAnywhere', 'en',
 			 *
 			 * Sample: 'q=twitter' or ['q=twitter', 'geocode=48.856667,2.350833,30km']
 			 */
-			searchParams: [null],
+			searchParams: ['q=galapagos', 'q=livinggalapagos', 'q=livinggalapagos13'],
 
 			/**
 			 * The number of tweets shown in the tweet feed. If this param is 0, no feed
